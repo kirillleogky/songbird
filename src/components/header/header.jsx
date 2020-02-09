7757777c4c1c5c01abf9af52141c65770322a381
Kirill Leogky <kirillmargheriti@gmail.com>
2020-02-09 12:32:58 +0300

    refactor: change the indentations and remake the component into functional

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from 'react'
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header_block">
-        <div className="header_block-logo"></div>
-        <div className="header_block-score">
-          Score:
-          <span className="header_block-score_curr" id="score">0</span>
-        </div>
-      </header>
-    );
-  }
+const Header = () => {
+  return (
+    <header className="header_block">
+      <div className="header_block-logo" />
+      <div className="header_block-score">
+        Score:
+        <span className="header_block-score_curr" id="score">
+          0
+        </span>
+      </div>
+    </header>
+  )
 }
 
-export default Header;
+export default Header
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Header from './components/header/header'
+import Header from './components/header/header';
+import './style.scss';
 
-ReactDOM.render(<Header />, document.getElementById('wrapper'))
+ReactDOM.render(<Header />, document.getElementById('wrapper'));
